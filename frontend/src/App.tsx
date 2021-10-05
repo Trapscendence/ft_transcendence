4b1f8ad8436e5c36f653ad1fe4874ac3a7045dbc
seohee-choi <choosehui@gmail.com>
2021-10-05 16:35:59 +0900

    tmp: mui 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './routes/HomePage';
 import LoginPage from './routes/LoginPage';
+import Navigation from './components/Navigation';
+import styled from "styled-components";
+
+
+const StyledDiv = styled.div`
+  display:flex;
+`;
 
 function App() {
+	//app css가 flex 할 수 있게 하셈~
   return (
     <BrowserRouter>
+    <StyledDiv>
+      <Navigation />
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/login" exact component={LoginPage} />
@@ -14,6 +24,8 @@ function App() {
         <Route path="/chat" exact component={LoginPage} />
         <Route path="/chat/:id" exact component={LoginPage} />
       </Switch>
+
+      </StyledDiv>
     </BrowserRouter>
   );
 }
