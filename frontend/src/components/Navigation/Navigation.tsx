47bd1f1bc7dcff0024063b14ca5b4609b3f371d4
moon9ua <koom6@naver.com>
2021-10-18 00:30:05 +0900

    feat: Nav 게임 매칭 MUI Mockup

--- frontend/src/components/Navigation/Navigation.tsx
+++ frontend/src/components/Navigation/Navigation.tsx
@@ -5,7 +5,7 @@ import {
   MoreHoriz,
   VideogameAsset,
 } from '@mui/icons-material';
-import { Box, Divider, Tab, Tabs } from '@mui/material';
+import { Box, CircularProgress, Divider, Tab, Tabs } from '@mui/material';
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 
@@ -17,6 +17,10 @@ function Navigation(): JSX.Element {
     history.push('/' + (path as string));
     setTabValue(newValue);
   }; // @mui/lab 에서 제공하는 페이지 이동 api가 있음. 어느게 성능이 더 좋을지 모르겠다. https://mui.com/components/tabs/#experimental-api 참고
+  const [loading, setLoading] = useState(false);
+  const onClickPlay = () => {
+    setLoading((value) => !value); // loading을 사용하는 toggle... 상태를 어떻게 잘 작성하지?
+  };
 
   return (
     <Box
@@ -43,7 +47,34 @@ function Navigation(): JSX.Element {
           <Tab aria-label="channel" icon={<Forum />} />
         </Tabs>
         <Divider />
-        <Tab icon={<VideogameAsset />} />
+
+        <Box
+          onClick={onClickPlay}
+          sx={{ position: 'relative', cursor: 'pointer' }}
+        >
+          <Tab
+            icon={<VideogameAsset />}
+            // disabled={loading}
+            // color={loading ? 'red' : 'yellow'}
+            sx={{ color: loading ? 'text.disabled' : '' }}
+          />
+          {/* loading에 따라 나타나게... */}
+          {/* 참고: https://mui.com/components/progress/#interactive-integration */}
+          {loading && (
+            <CircularProgress
+              color="secondary"
+              size={35}
+              sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                marginTop: '-17px',
+                marginLeft: '-17px',
+                zIndex: 1,
+              }}
+            />
+          )}
+        </Box>
       </Box>
       <Tab icon={<MoreHoriz />} />
     </Box>
