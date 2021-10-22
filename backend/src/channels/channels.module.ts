7d6e41cdcb4f5dcbda5fdd4a862ce2f72d119f38
moon9ua <koom6@naver.com>
2021-10-22 16:11:52 +0900

    feat: channels 모듈 init 및 @nestjs/graphql 설치

---
+++ backend/src/channels/channels.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { ChannelsService } from './channels.service';
+import { ChannelsResolver } from './channels.resolver';
+
+@Module({
+  providers: [ChannelsService, ChannelsResolver],
+})
+export class ChannelsModule {}
