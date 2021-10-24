420d0bc60d8089788d879c3fabf74d9047b46061
moon9ua <koom6@naver.com>
2021-10-25 02:36:32 +0900

    fix: User -> User! 수정, SQL문 들여쓰기 양식 변경

--- backend/src/users/users.resolver.ts
+++ backend/src/users/users.resolver.ts
@@ -1,5 +1,13 @@
-import { Query, Args, Int, Resolver, Mutation, ID, Info, ResolveField, Parent } from '@nestjs/graphql';
-import { identity } from 'rxjs';
+import {
+  Query,
+  Args,
+  Int,
+  Resolver,
+  Mutation,
+  ID,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { User } from './models/user.medel';
 import { UsersService } from './users.service';
 
@@ -7,18 +15,23 @@ import { UsersService } from './users.service';
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
+  /*
+   ** ANCHOR: User
+   */
+
   @Query((returns) => User)
   async user(@Args('id', { type: () => ID }) id: string): Promise<any> {
     return await this.usersService.getUser(id);
   }
 
-  @Query((returns) => [User], { nullable: true }) // 이렇게 하나?
+  // NOTE: [User!]! 반환하게 수정했습니다. 확인 후 코멘트 삭제해주세요. -gmoon
+  @Query((returns) => [User])
   async users(
     @Args('ladder') ladder: boolean,
     @Args('offset', { type: () => Int }) offset: number,
-    @Args('limit', { type: () => Int }) limit: number
-    ): Promise<any> {
-      return new Promise(()=>{}); // NOTE 임시
+    @Args('limit', { type: () => Int }) limit: number,
+  ): Promise<any> {
+    return new Promise(() => {});
   }
 
   @Mutation((returns) => User)
@@ -27,45 +40,50 @@ export class UsersResolver {
     return users[0];
   }
 
-  // SECTION
-  // 친구 관리... 여기서 하나?
+  /*
+   ** ANCHOR: Social
+   */
+  // NOTE: 나중에 분리할 수도...?
 
   @Mutation((returns) => User)
   async addFriend(
     @Args('user_id', { type: () => ID }) user_id: string,
-    @Args('friend_id', { type: () => ID }) friend_id: string
-    ): Promise<User> { // NOTE 여기서 할것인가?
-    return this.usersService.addFriend(user_id, friend_id)
+    @Args('friend_id', { type: () => ID }) friend_id: string,
+  ): Promise<User> {
+    return this.usersService.addFriend(user_id, friend_id);
   }
 
   @Mutation((returns) => User)
   async deleteFriend(
     @Args('user_id', { type: () => ID }) user_id: string,
-    @Args('frined_id', { type: () => ID }) friend_id: string
-    ): Promise<boolean> {
-    return this.usersService.deleteFriend(user_id, friend_id)
+    @Args('frined_id', { type: () => ID }) friend_id: string,
+  ): Promise<boolean> {
+    return this.usersService.deleteFriend(user_id, friend_id);
   }
 
   @Mutation((returns) => User)
   async addToBlackLIst(
     @Args('user_id', { type: () => ID }) user_id: string,
-    @Args('black_id', { type: () => ID }) black_id: string
-    ): Promise<User> {
-    return this.usersService.addToBlackList(user_id, black_id)
+    @Args('black_id', { type: () => ID }) black_id: string,
+  ): Promise<User> {
+    return this.usersService.addToBlackList(user_id, black_id);
   }
 
   @Mutation((returns) => User)
   async deleteFromBlackList(
     @Args('user_id', { type: () => ID }) user_id: string,
-    @Args('black_id', { type: () => ID }) black_id: string
-    ): Promise<User> { // NOTE 여기서 할것인가?
-    return this.usersService.deleteFromBlackList(user_id, black_id)
+    @Args('black_id', { type: () => ID }) black_id: string,
+  ): Promise<User> {
+    return this.usersService.deleteFromBlackList(user_id, black_id);
   }
+
+  /*
+   ** ANCHOR: ResolveField
+   */
 
   @ResolveField('friends', (returns) => [User])
   async getFriends(@Parent() user: User) {
     const { id } = user;
     return this.usersService.getFriend(id);
   }
-
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,106 +2,106 @@ import { Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 import { User } from './models/user.medel';
 import { schema } from 'src/utils/envs';
-import { resolve } from 'path/posix';
 
 @Injectable()
 export class UsersService {
   constructor(private databaseService: DatabaseService) {}
 
-  getFriend(id: string): Promise<User[]> {
-    return this.databaseService.executeQuery(`
-SELECT
-id,
-nickname,
-avatar,
-status_message,
-rank_score,
-site_role
-FROM
-  trap.user
-WHERE
-  id = ${id}
-INNER JOIN id ON trap.user.id = trap.friend.my_id;
-    `);
-  }
+  /*
+   ** ANCHOR: User
+   */
 
-
+  // NOTE: INNER JOIN ${schema}.friend f ON f.my_id = ${id}; // 혹시 이거 아닌가?
   async getUser(id: string): Promise<any> {
     const user = await this.databaseService.executeQuery(`
-SELECT
-  id,
-  nickname,
-  avatar,
-  status_message,
-  rank_score,
-  site_role
-FROM
-  ${schema}.user
-WHERE
-  id = ${id};`);
-    const friendArray = await this.databaseService.executeQuery(`
-SELECT
-  ${schema}.user.id
-FROM
-  ${schema}.user
-INNER JOIN ${schema}.user u ON u.id = ${id}
-INNER JOIN ${schema}.friend f ON u.id = f.my_id;
+      SELECT id, nickname, avatar, status_message, rank_score, site_role
+      FROM ${schema}.user
+      WHERE id = ${id};
     `);
+
+    // const friendArray = await this.databaseService.executeQuery(`
+    //   SELECT ${schema}.user.id
+    //   FROM ${schema}.user
+    //   INNER JOIN ${schema}.user u ON u.id = ${id}
+    //   INNER JOIN ${schema}.friend f ON u.id = f.my_id;
+    // `);
 
     return user[0];
   }
 
-//   async getUsers(): Promise<User[]> {
-//     return this.databaseService.executeQuery(`
-// SELECT (id, nickname, status_message, )
-//     `);
-//   }
-
-  async createUser(nickname: string): Promise<any> { // NOTE 일단 몰라서 Promise<any>로
-    return await this.databaseService.executeQuery(`
-INSERT INTO ${schema}.user( nickname, oauth_id, oauth_type )
-VALUES ( '${nickname}', 'mock_id', 'FORTYTWO' )
-RETURNING *;
-    `); // NOTE oauth_id, oauth_type, fta_secret는 일단 제외함. database.service에도 완성 전까지는 주석처리 해야할 듯?
+  async getUsers(): Promise<User[]> {
+    //     return this.databaseService.executeQuery(`
+    // SELECT (id, nickname, status_message, )
+    //     `);
+    return new Promise(() => {});
   }
+
+  createUser(nickname: string): Promise<any> {
+    return this.databaseService.executeQuery(`
+      INSERT INTO ${schema}.user( nickname, oauth_id, oauth_type )
+      VALUES ( '${nickname}', 'mock_id', 'FORTYTWO' )
+      RETURNING *;
+    `);
+  }
+
+  /*
+   ** ANCHOR: Social
+   */
 
   async addFriend(user_id: string, frined_id: string): Promise<any> {
     // const friend = await this.user.getFriendList.findOne({ frined_id });
     // if (friend) {
-      // return { ok: false, error: 'this user is already registered' };
+    // return { ok: false, error: 'this user is already registered' };
     // }
     // const exists = await this.user.findOne({ frined_id });
     // if (!exists) {
-      // return { ok: false, error: 'this user does not exist' };
+    // return { ok: false, error: 'this user does not exist' };
     // }
-    return (new Promise(() => {}));
+
+    return new Promise(() => {});
   }
 
   async deleteFriend(user_id: string, frined_id: string): Promise<any> {
     // const friend = await this.user.getFriendList.findOne({ frined_id });
     // if (!friend) {
-      // return { ok: false, error: 'friend not found' };
+    // return { ok: false, error: 'friend not found' };
     // }
-    return (new Promise(() => {}));
+
+    return new Promise(() => {});
   }
 
   async addToBlackList(user_id: string, black_id: string): Promise<any> {
     // const black = await this.user.getBlackList.findOne({ black_id });
     // if (black) {
-      // return { ok: false, error: 'this user is already added' };
+    // return { ok: false, error: 'this user is already added' };
     // }
     // const exists = await this.user.findOne({ black_id });
     // if (!exists) {
-      // return { ok: false, error: 'this user does not exist' };
+    // return { ok: false, error: 'this user does not exist' };
     // }
-    return (new Promise(() => {}));
+
+    return new Promise(() => {});
   }
 
   async deleteFromBlackList(user_id: string, black_id: string): Promise<any> {
     // const black = await this.user.getBlackList.findOne({ black_id });
     // if (!black) {
-      // return { ok: false, error: 'black user not found' };
+    // return { ok: false, error: 'black user not found' };
     // }
-    return (new Promise(() => {}));
+
+    return new Promise(() => {});
+  }
+
+  /*
+   ** ANCHOR: ResolveField
+   */
+
+  getFriend(id: string): Promise<User[]> {
+    return this.databaseService.executeQuery(`
+      SELECT id, nickname, avatar, status_message, rank_score, site_role
+      FROM ${schema}.user
+      WHERE id = ${id}
+      INNER JOIN id ON ${schema}.user.id = ${schema}.friend.my_id;
+    `);
   }
 }
