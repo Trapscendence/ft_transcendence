9f31d9dc7bfd12593987c26a36d69d6466d7bdb8
Seheon Yu <github@seheon.email>
2021-11-06 16:07:10 +0900

    style: Guard 코딩스타일 통일

--- backend/src/session/guard/session.guard.ts
+++ backend/src/session/guard/session.guard.ts
@@ -1,13 +1,11 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
 export class SessionGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
-    const session: Record<string, any> = context
-      .switchToHttp()
-      .getRequest().session;
-
-    console.log(session.id);
+    const request: any = context.switchToHttp().getRequest();
+    const session: Record<string, any> = request.session;
 
     if (session.uid === undefined) {
       return false;
