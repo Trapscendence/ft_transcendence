9199f7cebc7cbb197afa453440902ca5f2c9f076
Seheon Yu <github@seheon.email>
2021-11-12 10:25:49 +0900

    refactor: Merge jwt-guard and gql-jwt-guard #47

--- backend/src/channels/channels.resolver.ts
+++ backend/src/channels/channels.resolver.ts
@@ -17,10 +17,10 @@ import { ChannelsService } from './channels.service';
 import { Channel, ChannelNotify } from './models/channel.model';
 import { PubSub } from 'graphql-subscriptions';
 import { ChannelRoleGuard } from './guard/role.channel.guard';
-import { GqlJwtAuthGuard } from 'src/auth/guards/gql-jwt.guard';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { GqlUser } from 'src/auth/decorator/gql-user.decorator';
 
-@UseGuards(GqlJwtAuthGuard)
+@UseGuards(JwtAuthGuard)
 @UseGuards(ChannelRoleGuard)
 @Resolver((of) => Channel)
 export class ChannelsResolver {
