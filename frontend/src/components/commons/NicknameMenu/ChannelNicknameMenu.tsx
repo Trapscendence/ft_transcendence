5d2ba7e9a6d9e8762363d2a0d70eb91bd55a7e03
moon9ua <koom6@naver.com>
2021-11-16 14:45:36 +0900

    feat: delegate, relagate 구현 및 에러 처리 강화

--- frontend/src/components/commons/NicknameMenu/ChannelNicknameMenu.tsx
+++ frontend/src/components/commons/NicknameMenu/ChannelNicknameMenu.tsx
@@ -5,13 +5,18 @@ import { ListItemIcon, MenuItem } from '@mui/material';
 import { userIdVar } from '../../..';
 import {
   BAN_AND_KICK_USER,
+  DELEGATE_USER_ON_CHANNEL,
   GET_MY_CHANNEL_ROLE,
   MUTE_USER,
+  RELEGATE_USER_ON_CHANNEL,
 } from '../../../utils/gqls';
+import handleError from '../../../utils/handleError';
 import {
   BanAndKickUserResponse,
+  DelegateUserOnChannelResponse,
   GetMyChannelRoleResponse,
   MuteUserResponse,
+  RelegateUserOnChannelResponse,
 } from '../../../utils/responseModels';
 import ErrorAlert from '../ErrorAlert';
 
@@ -38,6 +43,7 @@ export default function ChannelNicknameMenu({
     MUTE_USER,
     {
       variables: { mute_time: 10000, user_id: id, channel_id: channelId }, // NOTE: 일단 시간은 고정... 추후 시간 조정하도록 구현
+      // variables: { mute_time: 10000, user_id: id }, // NOTE: 에러는 이런 식으로 발생
     }
   );
 
@@ -48,49 +54,75 @@ export default function ChannelNicknameMenu({
     }
   );
 
-  if (channelRoleError)
-    return (
-      <ErrorAlert
-        name="ChannelNicknameMenu: channelRoleError"
-        error={channelRoleError}
-      />
-    );
-  if (targetChannelRoleError)
-    return (
-      <ErrorAlert
-        name="ChannelNicknameMenu: targetChannelRoleError"
-        error={targetChannelRoleError}
-      />
-    );
-  if (muteError)
-    return (
-      <ErrorAlert name="ChannelNicknameMenu: muteError" error={muteError} />
-    );
-  if (banError)
-    return <ErrorAlert name="ChannelNicknameMenu: banError" error={banError} />;
+  const [delegateUser, { error: delegateError }] =
+    useMutation<DelegateUserOnChannelResponse>(DELEGATE_USER_ON_CHANNEL, {
+      variables: { user_id: id, channel_id: channelId },
+    });
 
-  if (
-    (channelRoleData && channelRoleData.user.channel_role === 'USER') ||
-    (targetChannelRoleData &&
-      targetChannelRoleData.user.channel_role !== 'USER')
-  ) {
+  const [relegateUser, { error: relegateError }] =
+    useMutation<RelegateUserOnChannelResponse>(RELEGATE_USER_ON_CHANNEL, {
+      variables: { user_id: id, channel_id: channelId },
+    });
+
+  const errorVar =
+    channelRoleError ||
+    targetChannelRoleError ||
+    muteError ||
+    banError ||
+    delegateError ||
+    relegateError;
+
+  if (errorVar)
+    return <ErrorAlert name="ChannelNicknameMenu" error={errorVar} />;
+
+  const isTargetAdmin = targetChannelRoleData?.user.channel_role !== 'USER';
+
+  const canDelegate =
+    channelRoleData?.user.channel_role === 'OWNER' &&
+    targetChannelRoleData?.user.channel_role === 'USER';
+
+  const canRelegate =
+    channelRoleData?.user.channel_role === 'OWNER' &&
+    targetChannelRoleData?.user.channel_role === 'ADMIN';
+
+  if (channelRoleData?.user.channel_role === 'USER') {
     return <></>;
-  } // NOTE: 내가 channel_role이 User이거나, 상대가 channel_role이 User가 아니면... ban, mute 프론트에서 막기
+  } // NOTE: 내가 channel_role이 User면 얼리 리턴
 
   return (
     <>
-      <MenuItem key={1} onClick={() => banUser()}>
-        <ListItemIcon>
-          <Forum fontSize="small" />
-        </ListItemIcon>
-        Ban
-      </MenuItem>
-      <MenuItem key={2} onClick={() => muteUser()}>
-        <ListItemIcon>
-          <Forum fontSize="small" />
-        </ListItemIcon>
-        Mute
-      </MenuItem>
+      {!isTargetAdmin && (
+        <>
+          <MenuItem onClick={() => handleError(banUser)}>
+            <ListItemIcon>
+              <Forum fontSize="small" />
+            </ListItemIcon>
+            Ban
+          </MenuItem>
+          <MenuItem onClick={() => handleError(muteUser)}>
+            <ListItemIcon>
+              <Forum fontSize="small" />
+            </ListItemIcon>
+            Mute
+          </MenuItem>
+        </>
+      )}
+      {canDelegate && (
+        <MenuItem onClick={() => handleError(delegateUser)}>
+          <ListItemIcon>
+            <Forum fontSize="small" />
+          </ListItemIcon>
+          Delegate admin
+        </MenuItem>
+      )}
+      {canRelegate && (
+        <MenuItem onClick={() => handleError(relegateUser)}>
+          <ListItemIcon>
+            <Forum fontSize="small" />
+          </ListItemIcon>
+          Relegate admin
+        </MenuItem>
+      )}
     </>
   );
 }
