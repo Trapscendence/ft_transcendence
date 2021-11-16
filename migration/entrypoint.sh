dbd7289170be2384e51d55204184ff13719ffd7c
moon9ua <koom6@naver.com>
2021-11-16 22:51:39 +0900

    tmp: db-migrate 오류시 재시도하지 않도록 수정

--- migration/entrypoint.sh
+++ migration/entrypoint.sh
@@ -2,8 +2,9 @@
 apk add npm
 npm install -g db-migrate db-migrate-pg
 sleep 3
-db-migrate $1 ||
-  while [ true ]
-    do echo 'Error on migration'
-    sleep 5
-  done
+# db-migrate $1 ||
+  # while [ true ]
+    # do echo 'Error on migration'
+    # sleep 5
+  # done
+db-migrate $1
