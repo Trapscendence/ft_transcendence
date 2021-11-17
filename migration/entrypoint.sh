3729ad242331ed557c877a35cfecd6de5de258fc
Seheon Yu <github@seheon.email>
2021-11-17 19:48:22 +0900

    fix: 마이그레이션 오류 해결

--- migration/entrypoint.sh
+++ migration/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-apk add npm
-npm install -g db-migrate db-migrate-pg
 sleep 3
-# db-migrate $1 ||
-#   while [ true ]
-#     do echo 'Error on migration'
-#     sleep 5
-#   done
-db-migrate $1
+db-migrate $1 ||
+  while [ true ]
+    do echo 'Error on migration'
+    sleep 5
+  done
