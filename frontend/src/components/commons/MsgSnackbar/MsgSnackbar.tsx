df2d7bb392bca841ef789f01e4ffd9137b7df535
moon9ua <koom6@naver.com>
2021-11-18 21:49:14 +0900

    styled: MsgSnackbar 미사용 워닝 제거

--- frontend/src/components/commons/MsgSnackbar/MsgSnackbar.tsx
+++ frontend/src/components/commons/MsgSnackbar/MsgSnackbar.tsx
@@ -1,5 +1,5 @@
 import { Alert, Snackbar } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 interface MsgSnackbarProps {
   msg: string;
@@ -10,7 +10,7 @@ export default function MsgSnackbar({
   msg,
   severity,
 }: MsgSnackbarProps): JSX.Element {
-  const [open, setOpen] = useState<boolean>(true);
+  const [open] = useState<boolean>(true);
 
   // const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
   //   if (reason === 'clickaway') {
