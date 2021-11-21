3872535704b2c935089c7826666f7868ac5153d3
moon9ua <koom6@naver.com>
2021-11-22 00:15:39 +0900

    tmp: JWT id가 1번만 생성되는 현상 수정

--- backend/src/auth/strategies/ft.strategy.ts
+++ backend/src/auth/strategies/ft.strategy.ts
@@ -3,6 +3,41 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, Profile, VerifyCallback } from 'passport-42';
 import { UsersService } from 'src/users/users.service';
 
+// @Injectable()
+// export class FTStrategy extends PassportStrategy(Strategy, '42') {
+//   constructor(private readonly usersService: UsersService) {
+//     super({
+//       clientID: process.env.FORTYTWO_APP_ID,
+//       clientSecret: process.env.FORTYTWO_APP_SECRET,
+//       callbackURL: process.env.FORTYTWO_REDIRECT_URI,
+//       profileFields: {
+//         id: function (obj) {
+//           return String(obj.obj);
+//         },
+//       },
+//     });
+//   }
+
+//   async validate(
+//     accessToken: string,
+//     refreshToken: string,
+//     profile: Profile,
+//     cb: VerifyCallback,
+//   ) {
+//     const oauth_id = profile.id;
+//     const user = await this.usersService.getOrCreateUserByOAuth(
+//       oauth_id,
+//       'FORTYTWO',
+//     );
+
+//     if (user) {
+//       return cb(null, { id: user.id });
+//     } else {
+//       return cb('Bad query result', null);
+//     }
+//   }
+// }
+
 @Injectable()
 export class FTStrategy extends PassportStrategy(Strategy, '42') {
   constructor(private readonly usersService: UsersService) {
@@ -12,7 +47,7 @@ export class FTStrategy extends PassportStrategy(Strategy, '42') {
       callbackURL: process.env.FORTYTWO_REDIRECT_URI,
       profileFields: {
         id: function (obj) {
-          return String(obj.obj);
+          return obj.obj;
         },
       },
     });
@@ -24,9 +59,9 @@ export class FTStrategy extends PassportStrategy(Strategy, '42') {
     profile: Profile,
     cb: VerifyCallback,
   ) {
-    const oauth_id = profile.id;
+    const oauth_id = profile.id ?? profile._json.id;
     const user = await this.usersService.getOrCreateUserByOAuth(
-      oauth_id,
+      String(oauth_id),
       'FORTYTWO',
     );
 
