3236db943d2cdbf88e0fcea03d42acced2d029be
moon9ua <koom6@naver.com>
2021-11-23 02:51:19 +0900

    style: 이전 방식 주석 제거

--- frontend/src/components/Game/Game.tsx
+++ frontend/src/components/Game/Game.tsx
@@ -1,9 +1,7 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
-import { useEffect } from 'react';
 import { Redirect, useLocation } from 'react-router';
 
-import { userIdVar } from '../..';
 import { GameType } from '../../utils/Apollo/schemaEnums';
 import ErrorAlert from '../commons/ErrorAlert';
 import LoadingBackdrop from '../commons/LoadingBackdrop';
@@ -61,71 +59,6 @@ export default function Game(): JSX.Element {
     `,
     { variables: { game_id } }
   );
-
-  // NOTE: 아래처럼 user의 game 정보 받아오는 방식으로 하려했으나... game을 못 얻어오는 경우가 발생한다.
-  // 만약 그런식으로 하려면 channel과 channelList처럼 한군데에서 정보를 받고, refetch해서 보내주는 식으로 해야할 듯?
-
-  // const {
-  //   data: gameData,
-  //   loading,
-  //   error,
-  //   refetch,
-  // } = useQuery<{
-  //   user: {
-  //     // id: string;
-  //     game: {
-  //       id: string;
-  //       game_type: GameType;
-  //       left_score: number;
-  //       right_score: number;
-  //       left_player: {
-  //         id: string;
-  //         nickname: string;
-  //       };
-  //       right_player: {
-  //         id: string;
-  //         nickname: string;
-  //       };
-  //       observers: {
-  //         id: string;
-  //         nickname: string;
-  //       }[];
-  //     };
-  //   };
-  // }>(
-  //   gql`
-  //     query GetGameByUserId($id: ID!) {
-  //       user(id: $id) {
-  //         # id
-  //         game {
-  //           id
-  //           game_type
-  //           left_score
-  //           right_score
-  //           left_player {
-  //             id
-  //             nickname
-  //             # NOTE: avatar가 아직 구현이 안되어있는 것 같아서...
-  //           }
-  //           right_player {
-  //             id
-  //             nickname
-  //           }
-  //           observers {
-  //             id
-  //             nickname
-  //           }
-  //         }
-  //       }
-  //     }
-  //   `,
-  //   {
-  //     variables: { id: userIdVar() },
-  //     // onCompleted: (d) => {
-  //     //   console.log('dddddddd....', d);
-  //     // },
-  //   }
-  // ); // TODO: 추후 확실해지면 분리
 
   if (loading) return <LoadingBackdrop loading={loading} />; // NOTE: loading일 때에는 data가 undefined이므로, 이런 경우(data에 따라 결과 분기하는 경우) loading 처리를 꼭 해줘야!
   if (error) return <ErrorAlert name="Game" error={error} />;
--- frontend/src/components/commons/RestrictRoute/RestrictRoute.tsx
+++ frontend/src/components/commons/RestrictRoute/RestrictRoute.tsx
@@ -1,14 +1,11 @@
-import { useQuery, useReactiveVar } from '@apollo/client';
+import { useReactiveVar } from '@apollo/client';
 import { Box } from '@mui/system';
-import gql from 'graphql-tag';
-import { useEffect } from 'react';
 import { Redirect, Route, RouteComponentProps } from 'react-router';
 
 import { userIdVar } from '../../..';
 import DirectMessage from '../../DirectMessage';
 import Navigation from '../../Navigation';
 import SocialDrawer from '../../SocialDrawer';
-import LoadingBackdrop from '../LoadingBackdrop';
 
 interface RestrictRouteProps {
   component: React.ComponentType<RouteComponentProps> | React.ComponentType; // 맞나?
@@ -22,36 +19,6 @@ function RestrictRoute({
 }: RestrictRouteProps): JSX.Element {
   const userId = useReactiveVar(userIdVar);
   if (!userId) return <Redirect to="/login" />;
-
-  // const {
-  //   data: gameData,
-  //   loading: gameLoading,
-  //   refetch,
-  // } = useQuery<{
-  //   user: {
-  //     id: string;
-  //     game: {
-  //       id: string;
-  //     };
-  //   };
-  // }>(
-  //   gql`
-  //     query getGameByUserId($id: ID!) {
-  //       user(id: $id) {
-  //         id
-  //         game {
-  //           id
-  //         }
-  //       }
-  //     }
-  //   `,
-  //   {
-  //     variables: { id: userIdVar() },
-  //   }
-  // ); // NOTE: 접속 끊기면 게임 끝난걸로 구현할 듯... 따라서 게임 재접속 등이 필요가 없어짐
-
-  // if (gameLoading) return <LoadingBackdrop loading={gameLoading} />;
-  // if (gameData?.user.game) return <Redirect to="/game" />;
 
   return (
     <Route
