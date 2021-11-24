ebc3f7a3dc903b041f5143d312e19b93c955d697
Seheon Yu <github@seheon.email>
2021-11-24 17:38:34 +0900

    feat: NestJS file server #130

--- storage/src/app.controller.ts
+++ storage/src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/storage')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('/upload')
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/storage/${file.filename}`;
   }
 }
--- storage/src/app.module.ts
+++ storage/src/app.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { randomUUID } from 'crypto';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './public',
+        filename: (req, file, callback) =>
+          callback(null, `${randomUUID()}${extname(file.originalname)}`),
+      }),
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/storage',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
