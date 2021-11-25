ff2236a1d7451479fdbaca924199ed8a5e0bec43
Jonghyun Lim <ezemango@gmail.com>
2021-11-26 00:56:38 +0900

    fix: setNickname logic error 수정

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -171,7 +171,7 @@ export class UsersService {
     `,
       [nickname],
     );
-    if (!existence.length) return false;
+    if (existence.length) return false;
 
     const array = await this.databaseService.executeQuery(
       `
