c7b0fa1050d4e4285b3c592977817afc97c5221a
SeheonYu <seyu@student.42seoul.kr>
2021-12-09 07:10:43 +0900

    feat: Delete profile service from storage #167

--- storage/src/app.controller.ts
+++ storage/src/app.controller.ts
@@ -4,43 +4,29 @@ import {
   Get,
   Param,
   Post,
-  Res,
-  Session,
   UploadedFile,
-  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { Response } from 'express';
 import { AppService } from './app.service';
-import { SessionGuard } from './session.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/storage')
-  @Get('/upload')
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/storage/:filename')
+  findFile(@Param('filename') filename: string) {
+    return this.appService.find(filename);
   }
 
+  @Post()
   @UseInterceptors(FileInterceptor('file'))
-  @Post('/upload/profile')
-  @UseGuards(SessionGuard)
-  async uploadFile(
-    @Session() session: { uid: string },
-    @Res() res: Response,
-    @UploadedFile() file: Express.Multer.File,
-  ) {
-    if (await this.appService.setUserProfileUrl(session.uid, file.filename))
-      res.status(201).end();
-    else res.status(500).end();
+  createFile(@UploadedFile() file: Express.Multer.File) {
+    return this.appService.create(file);
   }
 
-  @Delete('/delete/:filename')
-  async deleteFile(@Param('filename') filename: string, @Res() res: Response) {
-    if (this.appService.deleteFile(filename)) res.status(204).end();
-    else res.status(404).end();
+  @Delete('/:filename')
+  deleteFile(@Param('filename') filename: string) {
+    return this.appService.delete(filename);
   }
 }
--- storage/src/database/pg-pool.factory.ts
+++
@@ -1,7 +0,0 @@
-import { env } from 'src/utils/envs';
-import { Pool } from 'pg';
-
-export function databasePoolFactory() {
-  const { user, host, database, password, port } = env.database;
-  return new Pool({ user, host, database, password, port });
-}
